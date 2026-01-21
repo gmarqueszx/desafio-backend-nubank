cbb9d836bfd8524c22e5f93b70ac1909012ebc39
João Gabriel Marques <bielpesso@gmail.com>
2026-01-20 22:45:00 -0300

    initial commit

---
+++ src/main/java/space/gmarqueszx/desafio_api_nubank/DesafioApiNubankApplication.java
@@ -0,0 +1,13 @@
+package space.gmarqueszx.desafio_api_nubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioApiNubankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioApiNubankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/gmarqueszx/desafio_api_nubank/DesafioApiNubankApplicationTests.java
@@ -0,0 +1,13 @@
+package space.gmarqueszx.desafio_api_nubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiNubankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
